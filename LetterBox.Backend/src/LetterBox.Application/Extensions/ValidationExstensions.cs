33bdb0b2350a69ba6e703ee011c16bcd300c4d8b
hiraeth31 <beautiful_distress@outlook.com>
2025-10-21 19:21:51 +1000

    validation: extension method for fluentvalidation

---
+++ LetterBox.Backend/src/LetterBox.Application/Extensions/ValidationExstensions.cs
@@ -0,0 +1,21 @@
+﻿using LetterBox.Domain.Common;
+using CSharpFunctionalExtensions;
+using FluentValidation.Results;
+
+namespace LetterBox.Application.Extensions
+{
+    public static class ValidationExstensions
+    {
+        public static ErrorList ToErrorList(this ValidationResult validationResult)
+        {
+            var validationErrors = validationResult.Errors;
+
+            var errors = from validationError in validationErrors
+                         let errorMessage = validationError.ErrorMessage
+                         let error = Error.Deserialize(errorMessage)
+                         select Error.Validation(error.Code, errorMessage, validationError.PropertyName);
+
+            return errors.ToList();
+        }
+    }
+}
--- LetterBox.Backend/src/LetterBox.Domain/Common/Error.cs
+++ LetterBox.Backend/src/LetterBox.Domain/Common/Error.cs
@@ -2,6 +2,7 @@
 {
     public class Error
     {
+        public const string SEPARATOR = "||";
         public string Code { get; }
         public string Message { get; }
         public ErrorType Type { get; }
@@ -26,6 +27,26 @@
 
         public static Error Conflict(string code, string message)
             => new(code, message, ErrorType.Conflict);
+
+        public string Serialize()
+        {
+            return string.Join(SEPARATOR, Code, Message, Type);
+        }
+
+        public static Error Deserialize(string serialized)
+        {
+            var parts = serialized.Split(SEPARATOR);
+
+            if (parts.Length < 3)
+                throw new ArgumentException("Invalid serialized format");
+
+            if (Enum.TryParse<ErrorType>(parts[2], out var type) == false)
+            {
+                throw new ArgumentException("Invalid serialized format");
+            }
+
+            return new Error(parts[0], parts[1], type);
+        }
 
         public ErrorList ToErrorList() => new([this]);
     }
