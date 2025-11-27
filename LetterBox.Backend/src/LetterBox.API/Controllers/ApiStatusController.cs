a26f16e00c6458ca23777963cda8c88094ec864b
kirill <kirya.krotov.03@mail.ru>
2025-11-27 22:32:43 +1000

    feat: проверка работоспособности api

---
+++ LetterBox.Backend/src/LetterBox.API/Controllers/ApiStatusController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetterBox.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApiStatusController: ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API is working now";
+        }
+
+        /// <summary>
+        /// api метод <object> для проверки статуса
+        /// backend'a (без проверки на реализацию)
+        /// </summary>
+        /// <returns> Task<IActionResult> (_status, datetime.utcNow, _message) </returns>
+        [HttpGet("status")]
+        public async Task<IActionResult> PingStatus()
+        {
+            return Ok(new
+            {
+                status = "OK",
+                timestamp = DateTime.UtcNow,
+                message = "API доступен ✅ | API status is avaliable ✅"
+            });
+        }
+    }
+}
